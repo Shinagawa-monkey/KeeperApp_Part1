8dddf7b7de30ba3f55e9cb8c4f0eb4493fd77293
Shinagawa-monkey <59675339+Shinagawa-monkey@users.noreply.github.com>
2022-10-31 23:42:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,5 +15,4 @@ root.render(<App />);
 //<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
-
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
